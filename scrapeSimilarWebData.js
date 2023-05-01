fe64a4818474a84bb6c34350c84da56dc6bbf635
Shreenidhi H D <shreenidhihaagalagodu@gmail.com>
2023-05-01 18:55:18 +0530

    Without browser window

--- scrapeSimilarWebData.js
+++ scrapeSimilarWebData.js
@@ -1,38 +1,47 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
 
 async function scrapeSimilarWebData(url) {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
   });
   const page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 800 });
 
   await page.goto("https://www.similarweb.com/website/" + url + "/#overview", {
     waitUntil: "networkidle2",
   });
 
   try {
-    await page.waitForSelector("p[data-test='total-visits'] + p", { timeout: 20000 });
-    await page.waitForSelector("p[data-test='avg-visit-duration'] + p", { timeout: 20000 });
+    await page.waitForSelector("p[data-test='total-visits'] + p", { timeout: 30000 });
+    await page.waitForSelector("p[data-test='avg-visit-duration'] + p", { timeout: 30000 });
   } catch (e) {
     console.log("Timeout error:", e);
   }
 
-  const Similarweb_Monthly_Traffic = await page.$eval(
-    "p[data-test='total-visits'] + p",
-    (element) => element.textContent.trim()
-  ).catch(() => "No data");
+  try {
+    const Similarweb_Monthly_Traffic = await page.$eval(
+      "p[data-test='total-visits'] + p",
+      (element) => element.textContent.trim()
+    ).catch(() => "No data");
 
-  const Avg_Visit_Duration = await page.$eval(
-    "p[data-test='avg-visit-duration'] + p",
-    (element) => element.textContent.trim()
-  ).catch(() => "No data");
+    const Avg_Visit_Duration = await page.$eval(
+      "p[data-test='avg-visit-duration'] + p",
+      (element) => element.textContent.trim()
+    ).catch(() => "No data");
 
-  console.log("Similarweb_Monthly_Traffic:", Similarweb_Monthly_Traffic);
-  console.log("Avg_Visit_Duration:", Avg_Visit_Duration);
+    console.log("Similarweb_Monthly_Traffic:", Similarweb_Monthly_Traffic);
+    console.log("Avg_Visit_Duration:", Avg_Visit_Duration);
 
-  await browser.close();
+    await browser.close();
 
-  return { Website: url, Similarweb_Monthly_Traffic, Avg_Visit_Duration };
+    return { Website: url, Similarweb_Monthly_Traffic, Avg_Visit_Duration };
+  } catch (error) {
+    console.error("Scraping error:", error);
+    await browser.close();
+    return { Website: url, error: "Scraping failed" };
+  }
 }
 
 module.exports = scrapeSimilarWebData;
